51b61cad3d0085dbdb0f4d425f650f35f4724e0d
Habibullo <hhalimjonov1997@gmail.com>
2024-06-05 02:17:15 +0900

    BRR: Initial Setup for Burak

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("Executed 123")
+import moment from "moment";
+
+const currenTime = moment().format("YYYY MM DD")
+console.log(currenTime)
+
+const person: string = "Daniel";
+const count: number = 100 
