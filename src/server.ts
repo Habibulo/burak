07770bd6331885ca3b5f304cc7a38f2cf860919d
Habibullo <hhalimjonov1997@gmail.com>
2024-06-07 00:32:07 +0900

    fea: connect via mongoose

--- src/server.ts
+++ src/server.ts
@@ -17,16 +17,35 @@
 
 */
 
+/*
+    36-37-Dars
+    Darsimiz rejasi:
+        - Enviroinmental Variable lar integratsiyasini amalga oshiramiz
+        - Burak loyihasi uchun yangi database ni hosil qilamiz
+        - MongoDb ga mongoose orqali ulanamiz
+        - Loyihamizga express ni ornatamiz
+*/
 
-
-
+// 35 =>
 //import moment from "moment";  // <= bu Modeljs
 // CommonJs da biz => const moment = require('moment')
-import dotenv from "dotenv";
-dotenv.config()
-
-
+// import dotenv from "dotenv";
+// dotenv.config()
 // console.log("PORT:", process.env.PORT);
 // console.log(process.env.MONGO)
 
+
 // Cluster => Database=> Collection=> Document
+
+// 36=>
+
+import dotenv from "dotenv";
+dotenv.config()
+import mongoose from "mongoose";
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log("MongoDB succesfully connected")
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch((err) => console.log("error on connection MongoDB", err))
