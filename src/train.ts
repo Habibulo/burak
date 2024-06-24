f7daef6a9fa0810abfd75c5b3d7629aacf03c473
Habibullo <hhalimjonov1997@gmail.com>
2024-06-25 05:21:13 +0900

    fix: train task edited

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,26 @@
 /* 
-L-TASK: 
-
-Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
-MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
-
+  M-TASK: 
+    Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va 
+    array ichidagi har bir raqam uchun raqamni ozi va hamda 
+    osha raqamni kvadratidan tashkil topgan object hosil qilib, 
+    hosil bolgan objectlarni array ichida qaytarsin.
+    MASALAN: getSquareNumbers([1, 2, 3]) 
+    return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
 */
-const reverseSentence = (str: string): string => {
-    const arr = str.split(' ')
-    return  arr.map(ele => ele.split('').reverse().join('')).join(' ')
-  }
 
-  const result = reverseSentence("Hi my name is Daniel")
-  console.log(result);
-  
+interface NumberWithSquare {
+  number: number;
+  square: number;
+}
+
+function getSquareNumbers(arr: number[]): NumberWithSquare[] {
+  return arr.map(num => ({
+      number: num,
+      square: num * num
+  }));
+}
+
+// Example usage:
+const result = getSquareNumbers([5, 8, 3, 7]);
+console.log(result); 
+// Output: [{number: 5, square: 25}, {number: 8, square: 64}, {number: 3, square: 9}, {number: 7, square: 49}]
