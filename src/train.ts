9e9a5dfe39fcbe78d94989ac5b630ed5d9dec16f
Habibullo <hhalimjonov1997@gmail.com>
2024-07-09 19:23:25 +0500

    fix: train task yangilandi

--- src/train.ts
+++ src/train.ts
@@ -6,11 +6,35 @@ va osha numberlar orasidagi tushib qolgan sonni topib uni return qilsin
 MASALAN: missingNumber([3, 0, 1]) return 2 
 */
 
-function missingNumber(nums: number[]): number {
-    const n: number = nums.length;
-    const totalSum: number = n * (n + 1) / 2;
-    const sum: number = nums.reduce((acc, curr) => acc + curr, 0);
-    return totalSum - sum;
-}
-// Test the function
-console.log(missingNumber([3, 0, 1])); // Output: 2
+// function missingNumber(num: number[]) {
+//     let min = 0, max = 0, missNum = [];
+//     for (let i = 0; i < num.length; i++) {
+//         if (num[i] < min) {
+//             min = num[i];
+//         }
+//         else if (num[i] > max) {
+//             max = num[i];
+//         }
+//     }
+//     for (let i = min; i <= max; i++) {
+//         if (!num.includes(i)) {
+//             missNum.push(i);
+//         }
+//     }
+//     console.log(missNum);
+// }
+
+
+function missingNumber(arr: number[]) {
+    const missingNumbers: Number[] = []
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    const numSet = new Set(arr);
+    for (let i = min; i <= max; i++) {
+      if (!numSet.has(i)) {
+        missingNumbers.push(i);
+      }
+    }
+    return missingNumbers
+  }
+console.log(missingNumber([100, 10, 200]));
